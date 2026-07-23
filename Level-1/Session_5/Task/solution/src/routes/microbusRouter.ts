93e1aa5be7c9aee664f9ec171f12985dca05b777
IslamMostafaYassin <Islam.Mostafa.Yassin@gmail.com>
2026-07-23 22:51:30 +0300

    Add CRUD and filtering routes for microbus

--- Level-1/Session_5/Task/solution/src/routes/microbusRouter.ts
+++ Level-1/Session_5/Task/solution/src/routes/microbusRouter.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
-import { getAllBuses, getBusById } from '../controllers/microbusController';
+import {
+  getAllBuses,
+  getBusById,
+  createBus,
+  updateBus,
+  deleteBus,
+  filterByFare,
+  getRatingByRater
+} from '../controllers/microbusController';
 
 const router = Router();
+
+router.get('/filter', filterByFare);
+router.get('/rate/:id', getRatingByRater);
 
 router.get('/', getAllBuses);
 router.get('/:id', getBusById);
 router.post('/', createBus);
 router.put('/:id', updateBus);
+router.delete('/:id', deleteBus);
 
 export default router;
